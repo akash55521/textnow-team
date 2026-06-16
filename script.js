e488acf3c0ffedfd8419a38950cd9438f410c90a
akash55521 <mdrakibulislam55522@gmail.com>
2026-06-17 04:34:57 +0600

    Update script.js

--- script.js
+++ script.js
@@ -128,7 +128,7 @@ document.getElementById('digitalOrderForm').addEventListener('submit', async fun
         fetchOptions = {
             method: 'POST',
             headers: { 'Content-Type': 'application/json' },
-            body: JSON.stringify({ product_id: 'prd_eeeb28bc00cf', amount: quantity })
+            body: JSON.stringify({ product_id: 'fc4e9fb8e0c7e3b0457c620b', amount: quantity })
         };
     } else {
         alert("দয়া করে একটি সঠিক প্রোডাক্ট সিলেক্ট করুন (Hotmail/PlayStore)।");
